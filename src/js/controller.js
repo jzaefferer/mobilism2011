4c17b7d8fae348c144d57317c91f16340e8ef61e
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-05-13 21:37:38 +0200

    Alternative implementation, using jQuery

--- src/js/controller.js
+++
@@ -1,3 +0,0 @@
-;(function(){
-	dojo.read('init');
-})();
--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,17 @@
-require([
-	'lib/json_resource',
-	'views/page'
-], function(resource, pageView){
-	var forEach = dojo.forEach;
-	var pageView = new pageView();
-
-
-	// Global helpers
-	var valueToAttr = function(attr){
-		return function(node, value){
-			node.attr(attr, value);
-		}
-	};
-
-	window.into = {
-		html: valueToAttr('innerHTML'),
-		src: valueToAttr('src'),
-		value: valueToAttr('value')
-	};
-})
+$(function() {
+	function render(data) {
+		var row = $('<tr><td class="time"></td><td><div class="title"></div><div class="speaker"></div></td></tr>');
+		$.each(data, function(field, value) {
+			row.find("." + field).text(value);
+		});
+		return row;
+	}
+	var table = $("#page").delegate(".speaker", "click", function() {
+		$(this).text("Dude");
+	});
+	$.getJSON("data/schedule.json", function(data) {
+		$.each(data.objects, function(index, talk) {
+			render(talk).appendTo(table);
+		});
+	});
+});
